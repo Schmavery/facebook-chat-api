b3f637b6dbd5887ff6a26d15b05caa6e7e72e624
Benjamin San Souci <benjamin.sansouci@gmail.com>
2015-02-24 01:27:32 -0500

    removed for reals

--- tmp/Facebook_files/cZpKaf70pMZ.js
+++
@@ -1,5 +0,0 @@
-/*!CK:1588600518!*//*1424119050,*/
-
-if (self.CavalryLogger) { CavalryLogger.start_js(["6JCsB"]); }
-
-__d("ItunesDetector",["UserAgent_DEPRECATED"],function(a,b,c,d,e,f,g){function h(){var j=navigator.plugins;if(j&&j.length)for(var k=0;k<j.length;k++)if(j[k].name.indexOf("iTunes Application Detector")>-1)return true;return false;}var i={hasItunes:function(){if(g.osx()){return true;}else return h();}};e.exports=i;},null);
--- tmp/Facebook_files/p7vJjnPNuaa.js
+++
@@ -1,5 +0,0 @@
-/*!CK:784277195!*//*1424143996,*/
-
-if (self.CavalryLogger) { CavalryLogger.start_js(["9DjTy"]); }
-
-__d("isMessengerDotComURI",[],function(a,b,c,d,e,f){var g=new RegExp('(^|\\.)messenger\\.com$','i'),h=['https'];function i(j){if(j.isEmpty()&&j.toString()!=='#')return false;if(!j.getDomain()&&!j.getProtocol())return true;return (h.indexOf(j.getProtocol())!==-1&&g.test(j.getDomain()));}e.exports=i;},null);
