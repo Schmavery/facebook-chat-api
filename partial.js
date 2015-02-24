34aefe7c62144317d9c175f65bd9ca7ae37160a1
Benjamin San Souci <benjamin.sansouci@gmail.com>
2015-02-24 01:29:09 -0500

    removed more stuff

--- partial.js
+++
@@ -1,8 +0,0 @@
-function partial (fn) {
-  var args = Array.prototype.slice.call(arguments, 1);
-  return function() {
-    return fn.apply(this, args.concat(Array.prototype.slice.call(arguments, 0)));
-  };
-}
-
-module.exports = partial;
