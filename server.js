45b43777eaf4bc09c0e93cbf0eb10117ee9db8c1
Schmavery <elflord@live.ca>
2015-02-25 18:23:15 -0500

    Add welcome and help messages

--- server.js
+++ server.js
@@ -32,10 +32,16 @@ function startBot(api, chats) {
     // And set the global object
     currentChat = chats[chatid] = chats[chatid] || {
       lists: {},
-      scores: {}
+      scores: {},
+      firstChat: true
     };
     if(!currentChat.lists) currentChat.lists = {};
     if(!currentChat.scores) currentChat.scores = {};
+
+    if (currentChat.firstChat){
+      currentChat.firstChat = false;
+      api.sendMessage("Hey, type '/help' for some useful commands!", chatid);
+    }
 
     currentUsername = username;
     currentOtherUsernames = otherUsernames;
@@ -69,7 +75,8 @@ function startBot(api, chats) {
           [[/(hey marc$|marc\?)/i],["Sup", "Hey :D", "hey", "Me?", "yes?"]],
           [[/(sup|wassup|what's up|how are you)\??$/i], ["I'm tired", "Not much, you?", "Meh...", "I'm great, how about you?", "What's up with you?", "Nothing much, you?"]],
           [[/(who made you|who's your creator|where do you come from)/i], ["I'm a long story... About 24h long.", "I'm not too sure", "I never really asked myself this question."]],
-          [[/(\/sayit)/i], ["David's an idiot"]]
+          [[/(\/sayit)/i], ["David's an idiot"]],
+          [[/\/(help.*)/],["Try these commands:\n- /list help\n- hey marc\n- /ping\n- /slap [name]\n- /sayit\n- /xkcd keyword\n- name++\n- /score [name]\n- /topscore"]]
       ];
       for (var i = 0; i < possibilities.length; i++) {
           var possibleMatches = possibilities[i][0];
