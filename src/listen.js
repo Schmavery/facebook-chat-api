6065b98a4d94b184bb021a7e6ebef4ecd0329da0
Benjamin San Souci <benjamin.sansouci@gmail.com>
2015-06-05 23:15:56 -0400

    Updated listen to work with the new promise api.

--- src/listen.js
+++ src/listen.js
@@ -40,114 +40,108 @@ module.exports = function(mergeWithDefaults, api, ctx) {
     time.reportPullSent();
 
     // TODO: get the right URL to query...
-    utils.get("https://0-edge-chat.facebook.com/pull", ctx.jar, form, function(err, res, html) {
-      if(err) throw err;
+    utils.get("https://0-edge-chat.facebook.com/pull", ctx.jar, form)
+    .then(function(res) {
+      var html = res.body;
       if(!html) throw "html was null after request to https://0-edge-chat.facebook.com/pull with form " + JSON.stringify(form);
 
       time.reportPullReturned();
       var strData = utils.makeParsable(html);
       var info = null;
-      try {
-        info = JSON.parse(strData);
 
-        var now = Date.now();
-        log.info("Got answer in ", now - tmpPrev);
-        tmpPrev = now;
+      info = JSON.parse(strData);
 
-        if(info && info.t === "lb") {
-          time.reportPullReturned();
-          form.wtc = time.doSerialize();
-          form.sticky_token = info.lb_info.sticky;
-          form.sticky_pool = info.lb_info.pool;
-        }
+      var now = Date.now();
+      log.info("Got answer in ", now - tmpPrev);
+      tmpPrev = now;
 
-        if(info && info.t === 'fullReload') {
-          form.seq = info.seq;
-          delete form.sticky_pool;
-          delete form.sticky_token;
-          var form4 = mergeWithDefaults({
-            'lastSync' : ~~(lastSync/1000),
+      if(info && info.t === "lb") {
+        time.reportPullReturned();
+        form.wtc = time.doSerialize();
+        form.sticky_token = info.lb_info.sticky;
+        form.sticky_pool = info.lb_info.pool;
+      }
+
+      if(info && info.t === 'fullReload') {
+        form.seq = info.seq;
+        delete form.sticky_pool;
+        delete form.sticky_token;
+        var form4 = mergeWithDefaults({
+          'lastSync' : ~~(lastSync/1000),
+        });
+        utils.get("https://www.facebook.com/notifications/sync/", ctx.jar, form4)
+        .then(function(res) {
+          var cookies = res.headers['set-cookie'] || [];
+          cookies.map(function (c) {
+            ctx.jar.setCookie(c, "https://www.facebook.com");
           });
-          utils.get("https://www.facebook.com/notifications/sync/", ctx.jar, form4, function(err, res, html) {
-            var cookies = res.headers['set-cookie'] || [];
-            cookies.map(function (c) {
-              ctx.jar.setCookie(c, "https://www.facebook.com");
-            });
-            lastSync = Date.now();
-            var form6 = mergeWithDefaults({
-              'client' : 'mercury',
-              'folders[0]': 'inbox',
-              'last_action_timestamp' : ~~(Date.now() - 60)
-            });
-            utils.post("https://www.facebook.com/ajax/mercury/thread_sync.php", ctx.jar, form, function(err, res, html) {
-              log.info("thread sync --->", html);
-              currentlyRunning = setTimeout(api.listen, 1000, callback);
-            });
+          lastSync = Date.now();
+          var form6 = mergeWithDefaults({
+            'client' : 'mercury',
+            'folders[0]': 'inbox',
+            'last_action_timestamp' : ~~(Date.now() - 60)
           });
-          return;
-        }
-
-        if(info.ms) {
-          var atLeastOne = false;
-          info.ms.map(function parsePackets(v) {
-            switch (v.type) {
-              case "mercury":
-                if(!ctx.globalOptions.listenEvents) return;
-
-                v.actions.map(function(v2) {
-                  callback(null, utils.formatEvent(v2), stopListening);
-                });
-                break;
-              case "messaging":
-                if(v.event !== "deliver") return;
-
-                if(!ctx.globalOptions.selfListen && v.message.sender_fbid.toString() === ctx.userId) return;
-
-                atLeastOne = true;
-                callback(null, utils.formatMessage(v), stopListening);
-                break;
-            }
+          utils.post("https://www.facebook.com/ajax/mercury/thread_sync.php", ctx.jar, form)
+          .then(function(res) {
+            log.info("thread sync --->", res.body);
+            currentlyRunning = setTimeout(api.listen, 1000, callback);
           });
-
-          if(atLeastOne) {
-            // Send deliveryReceipt notification to the server
-            var formDeliveryReceipt = mergeWithDefaults();
-
-            for (var i = 0; i < info.ms.length; i++) {
-              if(info.ms[i].message && info.ms[i].message.mid) formDeliveryReceipt["[" + i + "]"] = info.ms[i].message.mid;
-            }
-
-            // If there's at least one, we do the post request
-            if(formDeliveryReceipt["[0]"]) {
-              utils.post("https://www.facebook.com/ajax/mercury/delivery_receipts.php", ctx.jar, formDeliveryReceipt, function(err, res, html) {
-              });
-            }
-          }
-          // info.ms = info.ms.map(utils.formatMessage);
-          // info.ms.sort(function(a, b) {
-          //   return a.timestamp - b.timestamp;
-          // });
-        }
-
-
-        if(info.seq) form.seq = info.seq;
-        if(info.tr) form.traceid = info.tr;
-      } catch (e) {
-        log.error("ERROR in listen --> ",e, strData);
-        callback(e, null, stopListening);
-        currentlyRunning = setTimeout(api.listen, Math.random() * 200 + 50, callback);
+        });
         return;
       }
+
+      if(info.ms) {
+        var atLeastOne = false;
+        info.ms.sort(function(a, b) {
+          return a.timestamp - b.timestamp;
+        }).map(function parsePackets(v) {
+          switch (v.type) {
+            case "mercury":
+              if(!ctx.globalOptions.listenEvents) return;
+
+              v.actions.map(function(v2) {
+                callback(null, utils.formatEvent(v2), stopListening);
+              });
+              break;
+            case "messaging":
+              if(v.event !== "deliver") return;
+
+              if(!ctx.globalOptions.selfListen && v.message.sender_fbid.toString() === ctx.userId) return;
+
+              atLeastOne = true;
+              callback(null, utils.formatMessage(v), stopListening);
+              break;
+          }
+        });
+
+        if(atLeastOne) {
+          // Send deliveryReceipt notification to the server
+          var formDeliveryReceipt = mergeWithDefaults();
+
+          for (var i = 0; i < info.ms.length; i++) {
+            if(info.ms[i].message && info.ms[i].message.mid) formDeliveryReceipt["[" + i + "]"] = info.ms[i].message.mid;
+          }
+
+          // If there's at least one, we do the post request
+          if(formDeliveryReceipt["[0]"]) {
+            utils.post("https://www.facebook.com/ajax/mercury/delivery_receipts.php", ctx.jar, formDeliveryReceipt);
+          }
+        }
+        // info.ms = info.ms.map(utils.formatMessage);
+        // info.ms.sort(function(a, b) {
+        //   return a.timestamp - b.timestamp;
+        // });
+      }
+
+
+      if(info.seq) form.seq = info.seq;
+      if(info.tr) form.traceid = info.tr;
       currentlyRunning = setTimeout(api.listen, Math.random() * 200 + 50, callback);
 
-      // // If any call to stopListening was made, do not call the callback
-      // if(shouldStop) return;
-      // if(info.ms) {
-      //   // Send all messages to the callback
-      //   for (var j = 0; j < info.ms.length; j++){
-      //     callback(null, info.ms[j], stopListening);
-      //   }
-      // }
+    }).catch(function(err) {
+      log.error("ERROR in listen --> ", err);
+      callback(err, null, stopListening);
+      currentlyRunning = setTimeout(api.listen, Math.random() * 200 + 50, callback);
     });
   };
 };
