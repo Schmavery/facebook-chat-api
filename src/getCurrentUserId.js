f22277364593e15535b1d2c71665368e8059a2de
Mark Hughes <mark@markeh.me>
2015-07-10 16:04:10 +1000

    Small fix

--- src/getCurrentUserId.js
+++ src/getCurrentUserId.js
@@ -8,16 +8,17 @@ module.exports = function(mergeWithDefaults, api, ctx) {
   return function getCurrentUserId(callback) {
     utils.get("https://www.facebook.com/me", ctx.jar)
     .then(function(resData) {
-      var splitData = resData.req._headers.cookie.split("; ");
+      var splitData = resData.req._headers.cookie.split(";");
 
       var i = 0;
-
+      
       while (i < splitData.length) {
         var d = (splitData[i]).split("=");
-        if(d[0] == "c_user") {
-          callback(null, d[1]);
+        if(d[0].trim() == "c_user") {
+          callback(d[1]);
           return;
         }
+        i++;
       }
     })
     .catch(function(err) {
