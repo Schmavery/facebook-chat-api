81aa1ab344ef3a0a3b3b897f5e2e5e59485c2955
Tomasz Piasecki <tomek@omega323.com>
2015-07-16 00:26:52 +0200

    add getThreadListAll

---
+++ src/getThreadListAll.js
@@ -0,0 +1,34 @@
+/*jslint node: true */
+"use strict";
+
+var utils = require("../utils");
+var log = require("npmlog");
+
+module.exports = function(mergeWithDefaults, api, ctx) {
+  return function getThreadListAll(callback) {
+    if(!callback) callback = function() {};
+
+    var thread_counter = 0,
+        all_threads = [];
+
+    var getThreadList = function() {
+      api.getThreadList(thread_counter, thread_counter + 19, function(err, threads) {
+        if (err) return callback(err);
+
+        all_threads = all_threads.concat(threads);
+
+        if (threads.length == 20) {
+          thread_counter += 20;
+          return getThreadList();
+        }
+
+        all_threads.sort(function(a, b) {
+          return (a.timestamp > b.timestamp) ? 1 : (a.timestamp < b.timestamp) ? -1 : 0;
+        })
+        return callback(err, all_threads);
+      });
+    }
+
+    return getThreadList();
+  };
+};
