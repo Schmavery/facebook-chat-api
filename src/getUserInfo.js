ddeed090d8953274158c6131941c136e536da85c
Benjamin San Souci <benjamin.sansouci@gmail.com>
2015-07-31 10:34:00 -0400

    Fix `getUserInfo` naming consistency. #53.

--- src/getUserInfo.js
+++ src/getUserInfo.js
@@ -4,6 +4,28 @@
 var utils = require("../utils");
 var log = require("npmlog");
 
+function formatData(data) {
+  var retObj = {};
+
+  for (var prop in data) {
+    if (data.hasOwnProperty(prop)) {
+      var innerObj = data[prop];
+      retObj[prop] = {
+        name: innerObj.name,
+        firstName: innerObj.firstName,
+        vanity: innerObj.vanity,
+        thumbSrc: innerObj.thumbSrc,
+        profileUrl: innerObj.uri,
+        gender: innerObj.gender,
+        type: innerObj.type,
+        isFriend: innerObj.is_friend,
+      }
+    }
+  }
+
+  return retObj;
+}
+
 module.exports = function(mergeWithDefaults, api, ctx) {
   return function getUserInfo(id, callback) {
     var form = mergeWithDefaults();
@@ -16,7 +38,7 @@ module.exports = function(mergeWithDefaults, api, ctx) {
     utils.get("https://www.facebook.com/chat/user_info/", ctx.jar, form)
     .then(utils.parseResponse)
     .then(function(resData) {
-      callback(null, resData.payload.profiles);
+      callback(null, formatData(resData.payload.profiles));
     })
     .catch(function(err) {
       log.error("Error in getUserInfo", err);
