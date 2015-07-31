a0c96cb7f8bf7c6240ad0f64dc464f5d19655c20
Benjamin San Souci <benjamin.sansouci@gmail.com>
2015-07-31 11:32:13 -0400

    Same as previous.

--- src/getAccessToken.js
+++ src/getAccessToken.js
@@ -3,6 +3,8 @@
 
 module.exports = function(mergeWithDefaults, api, ctx) {
   return function getAccessToken(callback) {
+    if(!callback) return log.error("getAccessToken: need callback");
+
     return callback(ctx.access_token);
   };
 };
