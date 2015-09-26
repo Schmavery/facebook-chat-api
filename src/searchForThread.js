d14bd4f283c873e624a6cfc9996b6a6a801d051d
Brendan Batliner <brendan.batliner@gmail.com>
2015-09-26 18:10:53 -0500

    Limit from 1 --> 21
    See http://i.imgur.com/7thEAws.png. The Facebook API uses limit: 21 when it hits search_threads.php. Unless it was intentional to only search for a single thread, I think it was a typo.

--- src/searchForThread.js
+++ src/searchForThread.js
@@ -12,7 +12,7 @@ module.exports = function(defaultFuncs, api, ctx) {
       client: 'web_messenger',
       query: name,
       offset: 0,
-      limit: 1,
+      limit: 21,
       index: 'fbid',
     };
 
