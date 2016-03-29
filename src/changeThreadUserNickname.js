7db7e6b306b1a9ee6a4a5852df6d7ad4cce231dc
GAMELASTER <gamelaster@outlook.com>
2016-03-29 22:57:50 +0200

    Fix of indentation and removed logging

--- src/changeThreadUserNickname.js
+++ src/changeThreadUserNickname.js
@@ -4,35 +4,33 @@ var utils = require("../utils");
 var log = require("npmlog");
 
 module.exports = function(defaultFuncs, api, ctx) {
-  return function changeThreadUserNickname(nickname, threadID, participantID, callback) {
-		
-    var form = {
-      'nickname': nickname,
-			'participant_id': participantID,
-      'thread_or_other_fbid': threadID
-    };
-		console.log(form);
+	return function changeThreadUserNickname(nickname, threadID, participantID, callback) {
 
-    defaultFuncs
-      .post("https://www.messenger.com/messaging/save_thread_nickname/?source=thread_settings&dpr=1", ctx.jar, form)
-      .then(utils.parseAndCheckLogin(ctx.jar, defaultFuncs))
-      .then(function(resData) {
-				console.log(resData);
+		var form = {
+			'nickname': nickname,
+			'participant_id': participantID,
+			'thread_or_other_fbid': threadID
+		};
+
+		defaultFuncs
+			.post("https://www.messenger.com/messaging/save_thread_nickname/?source=thread_settings&dpr=1", ctx.jar, form)
+			.then(utils.parseAndCheckLogin(ctx.jar, defaultFuncs))
+			.then(function(resData) {
 				if (resData.error === 1545014) {
-          throw {error: "Trying to change user nickname what isnt in thread"};
-        }
-        if (resData.error === 1357031) {
-          throw {error: "Trying to change user nickname of a chat that doesn't exist. Have at least one message in the thread before trying to change the user nickname."};
-        }
-        if (resData.error) {
-          throw resData;
-        }
-				
+					throw {error: "Trying to change user nickname wasnt in thread"};
+				}
+				if (resData.error === 1357031) {
+					throw {error: "Trying to change user nickname of a chat that doesn't exist. Have at least one message in the thread before trying to change the user nickname."};
+				}
+				if (resData.error) {
+					throw resData;
+				}
+
 				return callback();
-      })
+			})
 			.catch(function(err) {
-        log.error("Error in changeThreadUserNickname", err);
-        return callback(err);
-      });
-  };
+				log.error("Error in changeThreadUserNickname", err);
+				return callback(err);
+			});
+	};
 };
