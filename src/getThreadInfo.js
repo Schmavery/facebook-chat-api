46dfd9bb49ed5f49fd043abdbb6d705c0d4e2004
Allan W <me@allanwang.ca>
2016-07-20 20:01:23 -0400

    "initialTest"

--- src/getThreadInfo.js
+++ src/getThreadInfo.js
@@ -4,49 +4,58 @@ var utils = require("../utils");
 var log = require("npmlog");
 
 module.exports = function(defaultFuncs, api, ctx) {
-  return function getThreadInfo(threadID, callback) {
-    if(!callback) callback = function() {};
+    return function getThreadInfo(threadID, callback) {
+        if (!callback) callback = function() {};
 
-    var form = {
-      'client' : 'mercury'
-    };
+        var form = {
+            'client': 'mercury'
+        };
 
-    api.getUserInfo(threadID, function(err, userRes) {
-      if(err) {
-        return callback(err);
-      }
-      var key = (Object.keys(userRes).length > 0) ? "user_ids" : "thread_fbids";
-      form['threads['+key+'][0]'] = threadID;
+        api.getUserInfo(threadID, function(err, userRes) {
+            if (err) {
+                return callback(err);
+            }
+            var key = (Object.keys(userRes).length > 0) ? "user_ids" : "thread_fbids";
+            form['threads[' + key + '][0]'] = threadID;
 
-      if(ctx.globalOptions.pageId) form.request_user_id = ctx.globalOptions.pageId;
+            if (ctx.globalOptions.pageId) form.request_user_id = ctx.globalOptions.pageId;
 
-      defaultFuncs.post("https://www.facebook.com/ajax/mercury/thread_info.php", ctx.jar, form)
-        .then(utils.parseAndCheckLogin(ctx.jar, defaultFuncs))
-        .then(function(resData) {
-          if (resData.error) {
-            throw resData;
-          } else if (!resData.payload){
-            throw {error: "Could not retrieve thread Info."};
-          }
+            defaultFuncs.post("https://www.facebook.com/ajax/mercury/thread_info.php", ctx.jar, form)
+                .then(utils.parseAndCheckLogin(ctx.jar, defaultFuncs))
+                .then(function(resData) {
+                    if (resData.error) {
+                        throw resData;
+                    } else if (!resData.payload) {
+                        throw {
+                            error: "Could not retrieve thread Info."
+                        };
+                    }
 
-          var threadData = resData.payload.threads[0];
-          var userData = userRes[threadID];
-          var info = {
-            participantIDs: threadData.participants.map(id => id.split(':').pop()),
-            name: userData != null && userData.name != null ? userData.name : threadData.name,
-            snippet: threadData.snippet,
-            messageCount: threadData.message_count,
-            emoji: threadData.custom_like_icon,
-            nicknames: threadData.custom_nickname,
-            color: threadData.custom_color,
-          };
-          callback(null, info);
+                    var threadData = resData.payload.threads[0];
+                    var userData = userRes[threadID];
 
-        }).catch(function(err) {
-          log.error("Error in getThreadInfo", err);
-          return callback(err);
+                    if (threadData == null) {
+                        throw {
+                            error: "ThreadData is null"
+                        };
+                    }
+
+                    var info = {
+                        participantIDs: threadData.participants.map(id => id.split(':').pop()),
+                        name: userData != null && userData.name != null ? userData.name : threadData.name,
+                        snippet: threadData.snippet,
+                        messageCount: threadData.message_count,
+                        emoji: threadData.custom_like_icon,
+                        nicknames: threadData.custom_nickname,
+                        color: threadData.custom_color,
+                    };
+                    callback(null, info);
+
+                }).catch(function(err) {
+                    log.error("Error in getThreadInfo", err);
+                    return callback(err);
+                });
         });
-    });
 
-  };
+    };
 };
--- src/getThreadList.js
+++ src/getThreadList.js
@@ -4,40 +4,54 @@ var utils = require("../utils");
 var log = require("npmlog");
 
 module.exports = function(defaultFuncs, api, ctx) {
-  return function getThreadList(start, end, callback) {
-    if(!callback && utils.getType(end) !== 'Number') {
-      throw {error: "please pass an number as a second argument."};
-    }
-
-    if(!callback) {
-      throw {error: "getThreadList: need callback"};
-    }
-
-    if (end <= start) end = start + 20;
-
-    var form = {
-      'client' : 'mercury',
-      'inbox[offset]' : start,
-      'inbox[limit]' : end - start,
-    };
-
-    if(ctx.globalOptions.pageID) {
-      form.request_user_id = ctx.globalOptions.pageID;
-    }
-
-    defaultFuncs
-      .post("https://www.facebook.com/ajax/mercury/threadlist_info.php", ctx.jar, form)
-      .then(utils.parseAndCheckLogin(ctx.jar, defaultFuncs))
-      .then(function(resData) {
-        if (resData.error) {
-          throw resData;
+    return function getThreadList(start, end, pending, callback) {
+        if (utils.getType(callback) === 'Undefined') {
+            if (utils.getType(end) !== 'Number') {
+                throw {
+                    error: "please pass a number as a second argument."
+                };
+            } else if (utils.getType(pending) === 'Function') {
+                callback = pending;
+                pending = false; //default to inbox
+            } else if (utils.getType(pending) !== 'Boolean') {
+                throw {
+                    error: "please pass a Boolean as a third argument."
+                };
+            } else {
+                throw {
+                    error: "getThreadList: need callback"
+                };
+            }
         }
-        log.verbose("Response in getThreadList: " + JSON.stringify(resData.payload.threads));
-        return callback(null, (resData.payload.threads || []).map(utils.formatThread));
-      })
-      .catch(function(err) {
-        log.error("Error in getThreadList", err);
-        return callback(err);
-      });
-  };
+
+        if (end <= start) end = start + 20;
+
+        var messageBox = pending ? "pending" : "inbox";
+
+        var form = {
+            'client': 'mercury'
+        };
+
+        form[messageBox + '[offset]'] = start;
+        form[messageBox + '[limit]'] = end - start;
+
+        if (ctx.globalOptions.pageID) {
+            form.request_user_id = ctx.globalOptions.pageID;
+        }
+
+        defaultFuncs
+            .post("https://www.facebook.com/ajax/mercury/threadlist_info.php", ctx.jar, form)
+            .then(utils.parseAndCheckLogin(ctx.jar, defaultFuncs))
+            .then(function(resData) {
+                if (resData.error) {
+                    throw resData;
+                }
+                log.verbose("Response in getThreadList: " + JSON.stringify(resData.payload.threads));
+                return callback(null, (resData.payload.threads || []).map(utils.formatThread));
+            })
+            .catch(function(err) {
+                log.error("Error in getThreadList", err);
+                return callback(err);
+            });
+    };
 };
