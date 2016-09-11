5f5f828b9af25306982d2e32fc100aeeb9dd1942
ALiangLiang <david27072183@gmail.com>
2016-09-11 23:00:35 +0800

    update presence statuses to online

--- utils.js
+++ utils.js
@@ -646,7 +646,7 @@ function formatPresence(presence, userID) {
     type: "presence",
     timestamp: presence.la * 1000,
     userID: userID,
-    statuses: presence.p
+    online: (presence.ol === 0) ? true : false
   };
 }
 
