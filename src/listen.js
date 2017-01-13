fa030f4dcd397083c8e823f2473c1348d0ed6d98
Andrew Wong <andrew12345@live.com>
2017-01-13 23:50:52 +1100

    Code formatting

--- src/listen.js
+++ src/listen.js
@@ -148,28 +148,26 @@ module.exports = function(defaultFuncs, api, ctx) {
                 }
               });
               break;
-		  case 'delta':
-			if (v.delta.class !== "NewMessage" || ctx.globalOptions.pageID) {
-			  return;
-			}
-			(function resolveAttachmentUrl(i) {
-			  if (i == v.delta.attachments.length) {
-				var fmtMsg = utils.formatDeltaMessage(v);
-				if (!ctx.globalOptions.selfListen && fmtMsg.senderID === ctx.userID) return;
-				return globalCallback(null, fmtMsg);
-			  } else {
-				if (v.delta.attachments[i].mercury.attach_type == 'photo') {
-				  defaultFuncs
-					.get("https://www.facebook.com/mercury/attachments/photo/?photo_id=" + v.delta.attachments[i].fbid, ctx.jar, {})
-					.then(utils.parseAndCheckLogin(ctx.jar, defaultFuncs))
-					.then(function (resData) {
-					  if (!resData.error) v.delta.attachments[i].mercury.url = resData.jsmods.require[0][3][0]
-					  return resolveAttachmentUrl(i + 1);
-					})
-				}
-			  }
-			})(0)
-			break;
+			case 'delta':
+				if (v.delta.class !== "NewMessage" || ctx.globalOptions.pageID) return
+				(function resolveAttachmentUrl(i) {
+					if (i == v.delta.attachments.length) {
+						var fmtMsg = utils.formatDeltaMessage(v);
+						if (!ctx.globalOptions.selfListen && fmtMsg.senderID === ctx.userID) return;
+						return globalCallback(null, fmtMsg);
+					} else {
+						if (v.delta.attachments[i].mercury.attach_type == 'photo') {
+						defaultFuncs
+							.get("https://www.facebook.com/mercury/attachments/photo/?photo_id=" + v.delta.attachments[i].fbid, ctx.jar, {})
+							.then(utils.parseAndCheckLogin(ctx.jar, defaultFuncs))
+							.then(function (resData) {
+								if (!resData.error) v.delta.attachments[i].mercury.url = resData.jsmods.require[0][3][0]
+								return resolveAttachmentUrl(i + 1);
+							})
+						}
+					}
+				})(0)
+				break;
             case 'messaging':
               if (handleMessagingEvents(v)) {
                 return;
