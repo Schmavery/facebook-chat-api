69bdd30f49a442f1d43b6fd148a1224b741e97e9
Marek Kraus <gamelaster@outlook.com>
2017-03-21 20:06:45 +0100

    Adding forgotten formatDeltaReadReceipt definition

--- utils.js
+++ utils.js
@@ -748,6 +748,7 @@ module.exports = {
   formatEvent: formatEvent,
   formatPresence: formatPresence,
   formatTyp: formatTyp,
+  formatDeltaReadReceipt: formatDeltaReadReceipt,
   formatCookie: formatCookie,
   formatThread: formatThread,
   formatReadReceipt: formatReadReceipt,
