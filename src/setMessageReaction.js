2211491f5b3031337890d831e914967765877324
Marek Kraus <gamelaster@outlook.com>
2017-03-29 13:27:50 +0200

    Only supported reaction emojis and conversion from text

--- src/setMessageReaction.js
+++ src/setMessageReaction.js
@@ -11,6 +11,36 @@ module.exports = function(defaultFuncs, api, ctx) {
       callback = function() {};
     }
     
+    switch (reaction) {
+      case "\uD83D\uDE0D": //:heart_eyes:
+      case "\uD83D\uDE06": //:laughing:
+      case "\uD83D\uDE2E": //:open_mouth:
+      case "\uD83D\uDE22": //:cry:
+      case "\uD83D\uDE20": //:angry:
+      case "\uD83D\uDC4D": //:thumbsup:
+      case "\uD83D\uDC4E": //:thumbsdown:
+      case "":
+        //valid
+        break;
+      case ":heart_eyes:":
+        reaction = "\uD83D\uDE0D";
+      case ":laughing:":
+        reaction = "\uD83D\uDE06";
+      case ":open_mouth:":
+        reaction = "\uD83D\uDE2E";
+      case ":cry:":
+        reaction = "\uD83D\uDE22";
+      case ":angry:":
+        reaction = "\uD83D\uDE20";
+      case ":thumbsup:":
+        reaction = "\uD83D\uDC4D";
+      case ":thumbsdown:":
+        reaction = "\uD83D\uDC4E";
+      default:
+        return callback({error: "reaction is not valid emoji"});
+        break;
+    }
+    
     var variables = {
       data: {
         client_mutation_id: clientMutationId++,
