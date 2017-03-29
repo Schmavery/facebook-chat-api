a1dc255492ffb590d7240a79085d08eb9b3c96d1
Marek Kraus <gamelaster@outlook.com>
2017-03-29 20:25:38 +0200

    Implementing timestamp

--- src/getUserDay.js
+++ src/getUserDay.js
@@ -13,7 +13,7 @@ module.exports = function (defaultFuncs, api, ctx) {
                  'QueryFragment videoFragment : MessageVideo {playable_url,id} ' +
                  //'QueryFragment stickerFragment : Sticker {image {uri},animated_image {uri},id} ' + //I disabled stickers due it's probably not used now?
                  //'QueryFragment stickerMessageFragment : UserMessage {message {text},id} ' +
-                 'QueryFragment dayFragment : UserMessage {message_id,message_sender {id},message_source_data {message_source},blob_attachments {__typename as type,@imageFragment,@videoFragment}} ' +
+                 'QueryFragment dayFragment : UserMessage {message_id,message_sender {id}, timestamp, message_source_data {message_source},blob_attachments {__typename as type,@imageFragment,@videoFragment}} ' +
                  'QueryFragment dayStructureFragment : MessagesOfThreadConnection {count,edges {node {@dayFragment}}} ' +
                  'QueryFragment messagesFragment : MessageThread {messages.last(100) as userDays {@dayStructureFragment},id} ' +
                  'QueryFragment resultFragment : MessageThread {montage_thread {@messagesFragment}}';
@@ -22,6 +22,8 @@ module.exports = function (defaultFuncs, api, ctx) {
         if (err) {
           throw err;
         }
+        console.log(JSON.stringify(data, null, 2));
+        return;
         var days = [];
         if (data.response &&
             data.response[userID] &&
@@ -34,6 +36,7 @@ module.exports = function (defaultFuncs, api, ctx) {
               var formattedDay = {
                 "messageId": day.blob_attachments[0].id,
                 "type": day.blob_attachments[0].type,
+                "timestamp": day.timestamp * 1000,
               };
               if (day.blob_attachments[0].type == "MessageImage") {
                 formattedDay.uri = day.blob_attachments[0].image.uri;
