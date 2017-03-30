91b85e3d62aa35a1796abbbb2f343001a13ea3bd
Marek Kraus <gamelaster@outlook.com>
2017-03-30 20:43:20 +0200

    Implementing forwardAttachment

--- index.js
+++ index.js
@@ -82,6 +82,7 @@ function buildAPI(globalOptions, html, jar) {
     'createPoll',
     'deleteMessage',
     'deleteThread',
+    'forwardAttachment',
     'getCurrentUserID',
     'getFriendsList',
     'getThreadHistory',
