f82bbddff01dcf8a36c21c93017d99f3146a4178
bobland1 <bobland1@hotmail.co.uk>
2017-04-11 00:17:00 +0100

    Update listen.js
    Minor Changes - not finished

--- src/listen.js
+++ src/listen.js
@@ -121,21 +121,27 @@ module.exports = function(defaultFuncs, api, ctx) {
               break;
             case 'chatproxy-presence':
               // TODO: what happens when you're logged in as a page?
-              if(!ctx.globalOptions.updatePresence) {
-                return;
-              }
+              case 'buddylist':
+                 if(!ctx.globalOptions.updateBuddylist) {
+                   return;
+                 }
+                 break;
+              case 'presence':
+                 if(!ctx.globalOptions.updatePresence) {
+                   return;
+                 }
 
-              if (ctx.loggedIn) {
-                for(var userID in v.buddyList) {
-                  var formattedPresence = utils.formatProxyPresence(v.buddyList[userID], userID);
-                  if(formattedPresence != null)
-                  {
-                    globalCallback(null, formattedPresence);
-                  }
-                }
-                return;
-              }
-
+                 if (ctx.loggedIn) {
+                   for(var userID in v.buddyList) {
+                     var formattedPresence = utils.formatProxyPresence(v.buddyList[userID], userID);
+                     if(formattedPresence != null)
+                     {
+                       globalCallback(null, formattedPresence);
+                     }
+                   }
+                   return;
+                 }
+                 break;
               break;
             case 'buddylist_overlay':
               // TODO: what happens when you're logged in as a page?
