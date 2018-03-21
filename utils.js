9d1cc695ceb33a2ef2e931d13e47bf6470f6fd60
Rafał Pokrywka <rav_kr@interia.pl>
2018-03-22 00:14:22 +0100

    Fix response handling with multiple objects (#616) (#617)

--- utils.js
+++ utils.js
@@ -839,14 +839,18 @@ function getFrom(str, startToken, endToken) {
 }
 
 function makeParsable(html) {
-  var withoutForLoop = html.replace(/for\s*\(\s*;\s*;\s*\)\s*;\s*/, "");
+  let withoutForLoop = html.replace(/for\s*\(\s*;\s*;\s*\)\s*;\s*/, "");
 
   // (What the fuck FB, why windows style newlines?)
   // So sometimes FB will send us base multiple objects in the same response.
   // They're all valid JSON, one after the other, at the top level. We detect
   // that and make it parse-able by JSON.parse.
   //       Ben - July 15th 2017
-  var maybeMultipleObjects = withoutForLoop.split("}\r\n{");
+  //
+  // It turns out that Facebook may insert random number of spaces before
+  // next object begins (issue #616)
+  //       rav_kr - 2018-03-19
+  let maybeMultipleObjects = withoutForLoop.split(/\}\r\n *\{/);
   if (maybeMultipleObjects.length === 1) return maybeMultipleObjects;
 
   return "[" + maybeMultipleObjects.join("},{") + "]";
